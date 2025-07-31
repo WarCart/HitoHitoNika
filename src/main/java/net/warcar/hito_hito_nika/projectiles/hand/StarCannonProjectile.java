1ef8f76336caf7d0b582ec735125a286f37a0dde
Warcar <@imasanov2007@gmail,com>
2025-07-31 18:34:17 +0300

    dizzy to star canon

--- src/main/java/net/warcar/hito_hito_nika/projectiles/hand/StarCannonProjectile.java
+++ src/main/java/net/warcar/hito_hito_nika/projectiles/hand/StarCannonProjectile.java
@@ -1,11 +1,14 @@
 package net.warcar.hito_hito_nika.projectiles.hand;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import xyz.pixelatedw.mineminenomi.api.abilities.Ability;
 import xyz.pixelatedw.mineminenomi.api.abilities.ExplosionAbility;
+import xyz.pixelatedw.mineminenomi.api.effects.ModEffect;
 import xyz.pixelatedw.mineminenomi.api.helpers.AbilityHelper;
+import xyz.pixelatedw.mineminenomi.init.ModEffects;
 import xyz.pixelatedw.mineminenomi.particles.effects.gomu.GearSecondParticleEffect;
 
 public class StarCannonProjectile extends TrueKongGunProjectile {
@@ -16,7 +19,12 @@ public class StarCannonProjectile extends TrueKongGunProjectile {
         this.setDamageSource(this.getDamageSource().setPhysical());
         this.setBlocksAffectedLimit(250);
         this.onBlockImpactEvent = this::onBlockImpactEvent;
+        this.onEntityImpactEvent = this::onEntityImpact;
         this.onTickEvent = this::onTickEvent;
+    }
+
+    private void onEntityImpact(LivingEntity entity) {
+        entity.addEffect(new EffectInstance(ModEffects.DIZZY.get(), 60, 1));
     }
 
     private void onBlockImpactEvent(BlockPos hit) {
